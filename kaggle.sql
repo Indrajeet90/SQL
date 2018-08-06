5dfa13e37d770435515255408cb4049193337a20
Indrajeet Kumar <36623487+Indrajeet90@users.noreply.github.com>
2018-08-06 03:53:38 -0700

    Create kaggle.sql

---
+++ kaggle.sql
@@ -0,0 +1,5 @@
+# import package with helper functions 
+import bq_helper
+
+# create a helper object for this dataset
+bitcoin_blockchain = bq_helper.BigQueryHelper(active_project="bigquery-public-data",dataset_name="bitcoin_blockchain")
